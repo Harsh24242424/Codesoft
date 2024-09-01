05465d03288737d53369ceb6a8cc0fa7e1d53781
Harsh24242424 <gamerbaba2401@gmail.com>
2024-09-01 13:44:43 +0530

    Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+x1=int(input("Enter 1st number:\n"))
+x=(input("Enter operator:\n"))
+x2=int(input("Enter 2nd number:\n"))
+if(x=="+"):
+ print(x1+x2)
+elif(x=="-"):
+ print(x1-x2)
+elif(x=="*"):
+ print(x1*x2)
+elif(x=="/"):
+ print(x1/x2)
+elif(x=="%"):
+ print(x1%x2)
+else:
+ print("invalid operator")
+ 
+ 
